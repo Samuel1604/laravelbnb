03d1e6c2f2045c36d7f9f3646f8e368c06cb4ee7
github_username <you@domain.com>
2021-09-15 20:15:15 -0500

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,22 @@
 import router from "./routes";
 import VueRouter from "vue-router";
 import Vue from "vue";
+import Index from "./Index"
 require('./bootstrap');
 
 
 
 
 window.Vue = require('vue').default;
-Vue.component('example-component',
-    require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component',
+//     require('./components/ExampleComponent.vue').default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
     router,
+    components: {
+        index: Index,
+    }
 });
